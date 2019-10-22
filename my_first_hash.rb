ca7dd8772917ddc075c3da87fa965eee146593a5
Meagan Utsey <meagan.utsey@gmail.com>
2019-10-22 23:09:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
- my_hash = ["whale bone corsets" => 5,"porcelain vases" => 2,"oil paintings" => 3]
+ my_hash = [{"whale bone corsets" =>5,"porcelain vases" =>2,"oil paintings" =>3}]
 end
 
 
